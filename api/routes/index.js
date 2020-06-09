22b03d585931b84d6ef2c3b47d5cd190f6f9775d
haripriya <haripriyavarma98@gmail.com>
2020-06-09 17:26:28 +0530

    ajax

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login_form');
 });
 
@@ -12,6 +12,7 @@ router.get('/register', function (req, res, next) {
 
 router.post('/login', (req, res, next) => {
   console.log(req.body);
+  
 });
 
 module.exports = router;

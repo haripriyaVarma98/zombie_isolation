2c368f8fce44a4120370014a487d84e447b329f4
haripriya <haripriyavarma98@gmail.com>
2020-06-09 13:33:55 +0530

    created login form

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('login_form');
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('register');
+});
+
+router.post('/login', (req, res, next) => {
+  console.log(req.body);
 });
 
 module.exports = router;
